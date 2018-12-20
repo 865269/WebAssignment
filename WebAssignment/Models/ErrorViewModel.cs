8c2d8aa29b78a93ad5b46f804b94f713d0564f96
865269 <matthew.diggory@tesco.net>
2018-12-20 11:59:54 +0000

    Add project files.

---
+++ WebAssignment/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAssignment.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
