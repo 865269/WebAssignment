247380c7bbf813bc46eaeff4d336b9d90e87a34e
865269 <matthew.diggory@tesco.net>
2019-01-07 15:57:03 +0000

    added data annnotations

---
+++ WebAssignment/Data/Migrations/20190107150219_test.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAssignment.Data.Migrations
+{
+    public partial class test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CommentContent",
+                table: "Comment",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Post",
+                table: "BlogPost",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CommentContent",
+                table: "Comment",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Post",
+                table: "BlogPost",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 500);
+        }
+    }
+}
